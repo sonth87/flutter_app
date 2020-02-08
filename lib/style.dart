241c2b2b320cd55ee67c49de0f9e25e99202164a
sonth2 <v.sonth2@adayroi.com>
2020-02-08 13:01:05 +0700

    no message

---
+++ lib/style.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class Style {
+  navBarStyle(label, img) {
+    return new BottomNavigationBarItem(
+      icon: Image.asset(
+        'assets/images/home.png',
+        width: 20,
+        height: 20,
+        color: Color.fromRGBO(255, 255, 255, 0.3),
+        colorBlendMode: BlendMode.modulate,
+      ),
+      title: Container(
+        margin: EdgeInsets.only(top: 2.0),
+        child: Text('Home'),
+      ),
+    );
+  }
+}
